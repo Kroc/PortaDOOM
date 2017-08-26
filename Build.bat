d7aacb0dd8caef2979b0e6c08d3817b7b1d2e7c6
Kroc Camen <kroc@camendesign.com>
2017-08-26 08:49:08 +0100

    Add separate Cacowards 2015 build
    This bundles the Cacowards 2015 as its own PortaDOOM package -- Cacowards specific homepage is missing though. I will need to swap in and out alternate home pages.

--- Build.bat
+++ Build.bat
@@ -2,11 +2,34 @@
 PUSHD %~dp0
 
 REM # detect 32-bit or 64-bit Windows
-SET "WINBIT=32"
-IF /I "%PROCESSOR_ARCHITECTURE%" == "EM64T" SET "WINBIT=64"	& REM # Itanium
-IF /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" SET "WINBIT=64"	& REM # Regular x64
-IF /I "%PROCESSOR_ARCHITEW6432%" == "AMD64" SET "WINBIT=64"	& REM # 32-bit CMD on a 64-bit system (WOW64)
+SET WINBIT=32
+IF /I "%PROCESSOR_ARCHITECTURE%" == "EM64T" SET WINBIT=64	& REM # Itanium
+IF /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" SET WINBIT=64	& REM # Regular x64
+IF /I "%PROCESSOR_ARCHITEW6432%" == "AMD64" SET WINBIT=64	& REM # 32-bit CMD on a 64-bit system (WOW64)
 
+REM # select 7Zip executable
+IF %WINBIT% EQU 64 SET BIN_7ZA="bin\7za\7za_x64.exe"
+IF %WINBIT% EQU 32 SET BIN_7ZA="bin\7za\7za.exe"
+REM # select WinRAR executable (if present)
+REM # it's assumed you installed 64-bit WinRAR on a 64-bit system
+SET BIN_RAR="%PROGRAMFILES%\WinRAR\rar.exe"
+IF NOT EXIST %BIN_RAR% SET "BIN_RAR=" 
+REM # location of UPX executable
+SET BIN_UPX="bin\upx\upx.exe"
+
+REM # -ms=on    : solid-mode on (default)
+REM # -mqs=on   : sort solid-files by type
+REM # -mhc=on   : header compression on (default)
+REM # -mx=9     : maximum compression (level 9)
+REM # -myx=9    : maximum file analysis (level 9)
+SET "ZIP_MAX=-ms=on -mqs=on -mhc=on -mx=9 -myx=9"
+SET "ZIP_MIN=-ms=off -mhc=off -mx=0 -myx=0"
+
+REM # -s	: create solid archive
+REM # -m5	: best compression
+REM # -md1g	: use 1 GB dictionary (approx 6-10 GB used during compression!)
+SET "RAR_MAX=-s -m5 -md1g"
+SET "RAR_MIN=-m0"
 
 :menu
 CLS
@@ -35,72 +58,89 @@ IF "%$%" == "9" GOTO :do_dosmag_push
 
 GOTO :menu
 
+:select_compression
+REM ====================================================================================================================
+CLS
+ECHO:
+ECHO  Select Compression Level:
+ECHO:
+ECHO     [0]  None
+ECHO     [1]  Maximum
+ECHO:
+SET /P "$=Enter choice: "
+ECHO:
+
+SET CMPLVL=0
+IF "%$%" == "0" SET CMPLVL=0
+IF "%$%" == "1" SET CMPLVL=1
+
+GOTO:EOF
 
 
 :do_release
 REM ====================================================================================================================
 CLS & TITLE Creating PortaDOOM release...
+CALL :select_compression
 
-REM # select 7Zip executable
-IF "%WINBIT%" == "64" SET BIN_7ZA="bin\7za\7za_x64.exe"
-IF "%WINBIT%" == "32" SET BIN_7ZA="bin\7za\7za.exe"
-REM # select WinRAR executable (if present)
-REM # it's assumed you installed 64-bit WinRAR on a 64-bit system
-SET BIN_RAR="%PROGRAMFILES%\WinRAR\rar.exe"
-IF NOT EXIST %BIN_RAR% SET "BIN_RAR=" 
-REM # location of UPX executable
-SET BIN_UPX="bin\upx\upx.exe"
-
-
-ECHO [1] Clean Up
+ECHO * Clean Up
 REM --------------------------------------------------------------------------------------------------------------------
-DEL PortaDOOM.7z  >NUL 2>&1
-DEL PortaDOOM.rar  >NUL 2>&1
+DEL PortaDOOM\PortaDOOM.upx		  >NUL 2>&1
+DEL releases\PortaDOOM.7z                 >NUL 2>&1
+DEL releases\PortaDOOM.rar                >NUL 2>&1
+DEL releases\PortaDOOM_Cacowards2015.7z   >NUL 2>&1
+DEL releases\PortaDOOM_Cacowards2015.rar  >NUL 2>&1
 
-
-ECHO [2] Copy DOSmag executable and UPX compress it...
+ECHO * Copy DOSmag executable
 REM --------------------------------------------------------------------------------------------------------------------
-REM COPY /Y DOSmag\DOSmag.exe PortaDOOM\PortaDOOM.exe  >NUL 2>&1
+COPY /Y DOSmag\DOSmag.exe PortaDOOM\PortaDOOM.exe  >NUL 2>&1
 ECHO:
 
-REM %BIN_UPX% --ultra-brute PortaDOOM\PortaDOOM.exe
+IF %CMPLVL% EQU 1 (
+	ECHO * Compress DOSmag executable
+	ECHO:
+	%BIN_UPX% --ultra-brute PortaDOOM\PortaDOOM.exe
+	ECHO:
+)
 
-ECHO:
-ECHO [3] Make PortaDOOM.7z...
 REM --------------------------------------------------------------------------------------------------------------------
-ECHO:
 REM # a         : add to archive
 REM # -bso0	: disable message output
 REM # -bsp1	: display only progress
 REM # -r        : recurse sub-directories
-REM # -ms=on    : solid-mode on (default)
-REM # -mqs=on   : sort solid-files by type
-REM # -mhc=on   : header compression on (default)
-REM # -mx=9     : maximum compression (level 9)
-REM # -myx=9    : maximum file analysis (level 9)
 REM # -sfx...   : Create self-extracting archive  # -sfx7z.sfx 
 REM # -xr...	: exclude files (recursively)
-REM %BIN_7ZA% a -bso0 -bsp1 -r -ms=on -mqs=on -mhc=on -mx=9 -myx=9 -xr@bin\ignore.lst -- PortaDOOM.7z PortaDOOM
-REM %BIN_7ZA% a -bso0 -bsp1 -r -ms=on -mqs=on -mhc=on -mx=0 -myx=0 -xr@bin\ignore.lst -- PortaDOOM.7z PortaDOOM
+IF %CMPLVL% EQU 0 SET "ZIP_LVL=%ZIP_MIN%"
+IF %CMPLVL% EQU 1 SET "ZIP_LVL=%ZIP_MAX%"
 
-IF NOT %BIN_RAR% == "" (
 REM # a		: add to archive
 REM # -cfg-	: ignore RAR default profile and environment settings
 REM # -dh	: process shared files
-REM # -htb	: use BLAKE2 file hashes (stronger than CRC32)
 REM # -k	: lock the archive from further modification
-REM # -m5	: best compression
 REM # -ma5	: use RAR5 format (this is the default since v5.50)
-REM # -md1g	: use 1 GB dictionary (approx 6-10 GB used during compression!)
+REM # -htb	: use BLAKE2 file hashes (stronger than CRC32)
 REM # -ms...	: don't compress these files (DOOM-Crusher will have done its best already)
 REM # -qo-	: don't add quick-open info
 REM # -r	: recurse sub-folders
 REM # -rr3	: add 3% recovery record (protects against random corruption)
-REM # -s	: create solid archive
 REM # -ts-	: DO NOT save time accessed / modified / created to archive (saves space)
 REM # -x@...	: ignore files using the list file given
-%BIN_RAR% a -cfg- -dh -htb -k -m5 -ma5 -md1g -mspng;jpeg;jpg -qo- -r -rr3 -s -ts- -x@bin\ignore.lst PortaDOOM.rar -- PortaDOOM 
-)
+IF %CMPLVL% EQU 0 SET "RAR_LVL=%RAR_MIN%"
+IF %CMPLVL% EQU 1 SET "RAR_LVL=%RAR_MAX%"
+
+ECHO * Make PortaDOOM_Cacowards2015 ...
+
+REM # 7ZIP
+%BIN_7ZA% a -bso0 -bsp1 %ZIP_LVL% -xr@bin\ignore_7z.lst -i@bin\include_cacowards2015.lst -- releases\PortaDOOM_Cacowards2015.7z
+REM # WINRAR
+REM IF NOT %BIN_RAR% == "" %BIN_RAR% a -cfg- -dh -k -ma5 -htb -mspng;jpeg;jpg -qo- -rr3 -ts- %RAR_LVL% -x@bin\ignore_rar.lst releases\PortaDOOM_Cacowards2015.rar -- @bin\include_cacowards2015.lst
+
+PAUSE & EXIT /B
+ECHO * Make PortaDOOM ...
+
+REM # 7ZIP
+%BIN_7ZA% a -bso0 -bsp1 -r %ZIP_LVL% -xr@bin\ignore_7z.lst -- releases\PortaDOOM.7z PortaDOOM
+REM # WINRAR
+REM IF NOT %BIN_RAR% == "" %BIN_RAR% a -cfg- -dh -k -ma5 -htb -mspng;jpeg;jpg -qo- -r -rr3 -ts- %RAR_LVL% -x@bin\ignore_rar.lst releases\PortaDOOM.rar -- PortaDOOM
 
 ECHO:
 ECHO Complete.
