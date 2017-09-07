870e58b99a40f384f33b9990e5ee663604c037e5
Kroc Camen <kroc@camendesign.com>
2017-09-07 22:39:17 +0100

    Added "Shadow Of The Wool Ball" WAD page
    Very neat total conversion, a wolfenstein-like involving a cartoon hedgehog and hordes of evil kitties.

--- PortaDOOM/files/pwads/conversions/ShadowOfTheWoolBall/play.bat
+++ PortaDOOM/files/pwads/conversions/ShadowOfTheWoolBall/play.bat
@@ -1,2 +1,5 @@
 @ECHO OFF
-PUSHD "%~dp0" & CALL "..\..\..\play.bat" /REQ zdoom+hw /PWAD ShadowOfTheWoolBall.wad
+REM # requires a few manual settings originally provided in "ShadowOfTheWoolBall_Settings.cfg"
+PUSHD "%~dp0" & CALL "..\..\..\doom.bat" /SW gzdoom DOOM2 ShadowOfTheWoolBall.wad +wipetype 0 +movebob 0.015 +language=enu
+REM # original play.bat call, but doesn't yet support manual settings
+REM PUSHD "%~dp0" & CALL "..\..\..\play.bat" /REQ zdoom+sw /PWAD ShadowOfTheWoolBall.wad
