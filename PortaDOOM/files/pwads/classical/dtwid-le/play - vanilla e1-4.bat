f3b3b172cdb5477e404a5f56e93d43be83fe221f
Kroc Camen <kroc@camendesign.com>
2017-09-10 20:35:13 +0100

    Add "Doom the Way id Did - The Lost Episodes" WAD page

--- PortaDOOM/files/pwads/classical/dtwid-le/play - vanilla e1-4.bat
+++ PortaDOOM/files/pwads/classical/dtwid-le/play - vanilla e1-4.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PUSHD "%~dp0" & CALL "..\..\..\play.bat" /IWAD DOOM /REQ vanilla /PWAD DTWID-LE.wad -- DTWID_LE.deh
+PUSHD "%~dp0" & CALL "..\..\..\play.bat" /IWAD DOOM /REQ choco /PWAD DTWID-LE.wad -- DTWID-LE.deh
