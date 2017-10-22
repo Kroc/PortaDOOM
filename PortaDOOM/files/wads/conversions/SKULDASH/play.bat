61122a3d9f6c1f8b8c1434478b5be1c95615b4c1
Kroc Camen <kroc@camendesign.com>
2017-10-22 23:20:46 +0100

    Update Skulldash to Expanded Edition
    No more reliance on Z&3!

--- PortaDOOM/files/wads/conversions/SKULDASH/play.bat
+++ PortaDOOM/files/wads/conversions/SKULDASH/play.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PUSHD "%~dp0" & CALL "..\..\..\play.bat" /REQ zandronum-3 /PWAD SKULDASH-v1.0-hotfix.pk3 -- skulltag_actors.pk3 skulltag_data.pk3
+PUSHD "%~dp0" & CALL "..\..\..\play.bat" /REQ gzdoom /PWAD SkulldashExpandedEdition.pk3
