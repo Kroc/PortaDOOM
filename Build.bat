d4ef8edd57482d6da95579e1362fb63e9fea9f72
Kroc Camen <kroc@camendesign.com>
2018-01-18 10:28:05 +0000

    Committing before branch

--- Build.bat
+++ Build.bat
@@ -375,7 +375,7 @@ ECHO Pulling DOSmag updates from GitHub:
 ECHO (working copy must be clean!)
 ECHO:
 
-git subtree pull --prefix bin\DOSmag --squash dosmag master
+git subtree pull --prefix bin/DOSmag --squash dosmag master
 
 COPY /Y bin\DOSmag\DOSmag.exe PortaDOOM\PortaDOOM.exe >NUL
 
@@ -390,7 +390,7 @@ CLS
 ECHO Pushing DOSmag changes to GitHub:
 ECHO:
 
-git subtree push --prefix bin\DOSmag dosmag master
+git subtree push --prefix bin/DOSmag dosmag master
 
 ECHO:
 PAUSE
