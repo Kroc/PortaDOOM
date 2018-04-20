cbf89f933126741bdf49926c90a9d1b4a5e8f1c8
Kroc Camen <kroc@camendesign.com>
2018-04-20 17:43:26 +0100

    Clean up of "build.bat"

--- Build.bat
+++ Build.bat
@@ -3,16 +3,19 @@ PUSHD %~dp0
 
 REM # detect 32-bit or 64-bit Windows
 SET WINBIT=32
-IF /I "%PROCESSOR_ARCHITECTURE%" == "EM64T" SET WINBIT=64	& REM # Itanium
-IF /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" SET WINBIT=64	& REM # Regular x64
-IF /I "%PROCESSOR_ARCHITEW6432%" == "AMD64" SET WINBIT=64	& REM # 32-bit CMD on a 64-bit system (WOW64)
+IF /I "%PROCESSOR_ARCHITECTURE%" == "EM64T" SET WINBIT=64 & REM # Itanium
+IF /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" SET WINBIT=64 & REM # Regular x64
+REM # 32-bit CMD running on a 64-bit system (WOW64)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "AMD64" SET WINBIT=64
 
 REM # select 7Zip executable
-IF %WINBIT% EQU 64 SET "BIN_7ZA=bin\7za\7za_x64.exe"
-IF %WINBIT% EQU 32 SET "BIN_7ZA=bin\7za\7za.exe"
+IF %WINBIT% EQU 64 SET BIN_7ZA="%~dp0bin\7za\7za_x64.exe"
+IF %WINBIT% EQU 32 SET BIN_7ZA="%~dp0bin\7za\7za.exe"
 REM # location of UPX executable
-SET BIN_UPX="bin\upx\upx.exe"
+REM # (for compression EXE files)
+SET BIN_UPX="%~dp0bin\upx\upx.exe"
 
+REM # define ZIP compression profiles:
 REM # -ms=on    : solid-mode on (default)
 REM # -mqs=on   : sort solid-files by type
 REM # -mhc=on   : header compression on (default)
@@ -41,6 +44,9 @@ ECHO:
 
 SET "P="
 SET /P "$=Enter choice: "
+
+IF NOT EXIST "build" MKDIR "build"  >NUL 2>&1
+
 IF "%$%" == "1" GOTO :do_release_menu
 IF "%$%" == "2" GOTO :do_dosmag_copy
 IF "%$%" == "3" GOTO :do_upx
@@ -50,7 +56,8 @@ IF "%$%" == "9" GOTO :do_dosmag_push
 GOTO :menu
 
 :select_compression
-REM ====================================================================================================================
+REM ============================================================================
+REM # presents a menu to select the desired compression level
 ECHO:
 ECHO  Select Compression Level:
 ECHO:
@@ -63,13 +70,6 @@ SET CMPLVL=0
 IF "%$%" == "0" SET CMPLVL=0
 IF "%$%" == "1" SET CMPLVL=1
 
-REM # a         : add to archive
-REM # -bso0     : disable message output
-REM # -bsp1     : display only progress
-REM # -r        : recurse sub-directories
-REM # -stl      : sets the archive's timestamp to that of the latest file
-REM # -sfx...   : Create self-extracting archive  # -sfx7z.sfx 
-REM # -xr...    : exclude files (recursively)
 IF %CMPLVL% EQU 0 SET "ZIP_LVL=%ZIP_MIN%"
 IF %CMPLVL% EQU 1 SET "ZIP_LVL=%ZIP_MAX%"
 
@@ -88,7 +88,7 @@ GOTO:EOF
 
 
 :do_release_menu
-REM ====================================================================================================================
+REM ============================================================================
 CLS & TITLE PortaDOOM Build Tools:
 ECHO:
 ECHO  Select Release to Build:
@@ -119,7 +119,7 @@ GOTO :menu
 
 
 :do_release_all
-REM ====================================================================================================================
+REM ============================================================================
 CLS & TITLE Creating PortaDOOM release...
 CALL :select_compression
 
@@ -137,9 +137,10 @@ PAUSE
 EXIT /B
 
 ECHO * Make PortaDOOM ...
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 REM # 7ZIP
-"%BIN_7ZA%" a -bso0 -bsp1 -r %ZIP_LVL% -stl ^
+%BIN_7ZA% a ^
+	-bso0 -bsp1 -r %ZIP_LVL% -stl ^
 	-xr@bin\ignore.lst ^
 	-- build\PortaDOOM.7z PortaDOOM
 
@@ -151,14 +152,14 @@ EXIT /B
 
 
 :do_release_cacowards2017
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM release...
 CALL :select_compression
 CALL :do_cacowards2017
 PAUSE & GOTO:EOF
 
 :do_cacowards2017
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_Cacowards2017 ...
 
 REM # the archive will be built without a base folder
@@ -170,10 +171,10 @@ COPY "pages\PortaDOOM Cacowards 2017.dosmag" "pages\Home #01.dosmag"  >NUL 2>&1
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 REM # 7ZIP
-"..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_LVL% -stl ^
-	-xr@..\bin\ignore.lst ^
-	-i@..\bin\include_cacowards2017.lst ^
-	-- ..\build\PortaDOOM_Cacowards2017.7z
+CALL :zip ^
+	..\build\PortaDOOM_Cacowards2017.7z ^
+	..\bin\include_cacowards2017.lst ^
+	..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 REM # restore the original home page
@@ -186,14 +187,14 @@ GOTO:EOF
 
 
 :do_release_cacowards2016
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM release...
 CALL :select_compression
 CALL :do_cacowards2016
 PAUSE & GOTO:EOF
 
 :do_cacowards2016
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_Cacowards2016 ...
 
 REM # the archive will be built without a base folder
@@ -205,10 +206,10 @@ COPY "pages\PortaDOOM Cacowards 2016.dosmag" "pages\Home #01.dosmag"  >NUL 2>&1
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 REM # 7ZIP
-"..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_LVL% -stl ^
-	-xr@..\bin\ignore.lst ^
-	-i@..\bin\include_cacowards2016.lst ^
-	-- ..\build\PortaDOOM_Cacowards2016.7z
+CALL :zip ^
+	..\build\PortaDOOM_Cacowards2016.7z ^
+	..\bin\include_cacowards2016.lst ^
+	..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 REM # restore the original home page
@@ -221,14 +222,14 @@ GOTO:EOF
 
 
 :do_release_cacowards2015
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM release...
 CALL :select_compression
 CALL :do_cacowards2015
 PAUSE & GOTO:EOF
 
 :do_cacowards2015
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_Cacowards2015 ...
 
 REM # the archive will be built without a base folder
@@ -240,10 +241,10 @@ COPY "pages\PortaDOOM Cacowards 2015.dosmag" "pages\Home #01.dosmag"  >NUL 2>&1
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 REM # 7ZIP
-"..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_LVL% -stl ^
-	-xr@..\bin\ignore.lst ^
-	-i@..\bin\include_cacowards2015.lst ^
-	-- ..\build\PortaDOOM_Cacowards2015.7z
+CALL :zip ^
+	..\build\PortaDOOM_Cacowards2015.7z ^
+	..\bin\include_cacowards2015.lst ^
+	..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 REM # restore the original home page
@@ -256,16 +257,16 @@ GOTO:EOF
 
 
 :do_release_5yearsofdoom
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM release...
 CALL :select_compression
 CALL :do_5yearsofdoom
 PAUSE & GOTO:EOF
 
 :do_5yearsofdoom
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_5YearsOfDoom ...
-DEL build\PortaDOOM_5YearsOfDoom.7z	>NUL 2>&1
+DEL build\PortaDOOM_5YearsOfDoom.7z  >NUL 2>&1
 
 REM # the archive will be built without a base folder
 PUSHD PortaDOOM
@@ -276,10 +277,10 @@ COPY "pages\PortaDOOM Cacowards 5 Years of Doom.dosmag" "pages\Home #01.dosmag" 
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 REM # 7ZIP
-"..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_LVL% -stl ^
-	-xr@..\bin\ignore.lst ^
-	-i@..\bin\include_cacowards5years.lst ^
-	-- ..\build\PortaDOOM_5YearsOfDoom.7z
+CALL :zip ^
+	..\build\PortaDOOM_5YearsOfDoom.7z ^
+	..\bin\include_cacowards5years.lst ^
+	..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 REM # restore the original home page
@@ -292,16 +293,16 @@ GOTO:EOF
 
 
 :do_release_psxdoomtc
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM release...
 CALL :select_compression
 CALL :do_psxdoomtc
 PAUSE & GOTO:EOF
 
 :do_psxdoomtc
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_PSXDOOMTC ...
-DEL build\PortaDOOM_PSXDOOMTC.7z	>NUL 2>&1
+DEL build\PortaDOOM_PSXDOOMTC.7z  >NUL 2>&1
 
 REM # the archive will be built without a base folder
 PUSHD PortaDOOM
@@ -312,10 +313,10 @@ COPY "pages\PortaDOOM PSX DOOM TC.dosmag" "pages\Home #01.dosmag"  >NUL 2>&1
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 REM # 7ZIP
-"..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_LVL% -stl ^
-	-xr@..\bin\ignore.lst ^
-	-i@..\bin\include_psxdoomtc.lst ^
-	-- ..\build\PortaDOOM_PSXDOOMTC.7z
+CALL :zip ^
+	..\build\PortaDOOM_PSXDOOMTC.7z ^
+	..\bin\include_psxdoomtc.lst ^
+	..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 REM # restore the original home page
@@ -328,14 +329,14 @@ GOTO:EOF
 
 
 :do_release_launcher
-REM ====================================================================================================================
+REM ============================================================================
 TITLE Creating PortaDOOM Launcher release...
 ECHO:
 CALL :do_launcher
 PAUSE & GOTO:EOF
 
 :do_launcher
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 ECHO * Make PortaDOOM_Launcher ...
 DEL build\PortaDOOM_Launcher.7z  >NUL 2>&1
 
@@ -343,11 +344,12 @@ REM # the archive will be built without a base folder
 REM # and without the PortaDOOM executable / pages
 PUSHD PortaDOOM\files
 
-REM # Launcher is always maximum compression as it doesn't use the PortaDOOM executable
-"..\..\%BIN_7ZA%" a -bso0 -bsp1 %ZIP_MAX% -stl ^
-	-xr@..\..\bin\ignore.lst ^
-	-i@..\..\bin\include_launcher.lst ^
-	-- ..\..\build\PortaDOOM_Launcher.7z
+REM # Launcher is always maximum compression
+REM # as it doesn't use the PortaDOOM executable
+CALL :zip ^
+	..\..\build\PortaDOOM_Launcher.7z ^
+	..\..\bin\include_launcher.lst ^
+	..\..\bin\ignore.lst
 IF ERRORLEVEL 1 PAUSE
 
 POPD
@@ -355,7 +357,7 @@ GOTO:EOF
 
 
 :do_dosmag_copy
-REM ====================================================================================================================
+REM ============================================================================
 CLS
 COPY /Y bin\DOSmag\DOSmag.exe PortaDOOM\PortaDOOM.exe
 
@@ -365,7 +367,7 @@ EXIT /B
 
 
 :do_dosmag_pull
-REM ====================================================================================================================
+REM ============================================================================
 REM # updates DOSmag from the GitLab repo
 
 REM # TODO: check for existence of git
@@ -385,7 +387,7 @@ EXIT /B
 
 
 :do_dosmag_push
-REM ====================================================================================================================
+REM ============================================================================
 CLS
 ECHO Pushing DOSmag changes to GitHub:
 ECHO:
@@ -397,7 +399,7 @@ PAUSE
 EXIT /B
 
 :do_upx
-REM ====================================================================================================================
+REM ============================================================================
 CLS & TITLE UPX Compress DOOM Engines...
 ECHO:
 ECHO Compress DOOM Engines using UPX:
@@ -420,11 +422,30 @@ ECHO:
 PAUSE & GOTO:EOF
 
 :upx
-REM --------------------------------------------------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
 REM # check if the file is already compressed
-FOR /F "delims=" %%G IN ('bin\upx\upx.exe -qqq -l "%EXE_FILE%"') DO @SET EXE_INFO=%%G
+FOR /F "delims=" %%G IN ('%BIN_UPX% -qqq -l "%EXE_FILE%"') DO @SET EXE_INFO=%%G
 REM # if not compressed, run UPX on the executable
 IF "%EXE_INFO%" == "" (
-	bin\upx\upx.exe --all-methods --all-filters --ultra-brute "%EXE_FILE%"
+	%BIN_UPX% --all-methods --all-filters --ultra-brute "%EXE_FILE%"
 )
 GOTO:EOF
+
+:zip
+REM ----------------------------------------------------------------------------
+REM # run a zip-compression command;
+REM # assumes `ZIP_LVL` has been set
+REM #
+REM #	%1	= archive path
+REM #	%2	= include list, e.g. "include.lst"
+REM #	%3	= ignore list, e.g. "ignore.lst"
+
+REM # a         : add to archive
+REM # -bso0     : disable message output
+REM # -bsp1     : display only progress
+REM # -r        : recurse sub-directories
+REM # -stl      : sets the archive's timestamp to that of the latest file
+REM # -xr...    : exclude files (recursively)
+%BIN_7ZA% a -bso0 -bsp1 %ZIP_LVL% -stl -xr@%3 -i@%2 -- %1
+REM # return the error code from 7Zip
+EXIT /B %ERRORLEVEL%
