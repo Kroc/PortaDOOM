fc73f2e7cd1342213cd930286318180fc1580673
Kroc Camen <kroc@camendesign.com>
2018-08-06 12:28:22 +0100

    Move "bin\launcher" to root
    The launcher will be a major part of PortaDOOM going forward

--- launcher/test_launcher.bat
+++ launcher/test_launcher.bat
@@ -3,7 +3,7 @@
 REM # switch to the directory of this script
 PUSHD "%~dp0"
 REM # compile Launher.exe
-..\qb64\qb64.exe -x -e -o ..\launcher\launcher.exe ..\launcher\launcher.qb64
+..\bin\qb64\qb64.exe -x -e -o "..\..\launcher\launcher.exe" "..\..\launcher\launcher.qb64"
 REM # if that errored, pause to be able to show the error message
 IF ERRORLEVEL 1 POPD & PAUSE & GOTO:EOF
 
