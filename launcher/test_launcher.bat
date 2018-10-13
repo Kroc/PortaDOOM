4e597d20b9ab7ca8ad0d793580e402af356c0a69
Kroc Camen <kroc@camendesign.com>
2018-10-13 21:09:43 +0100

    Add `auto` files for games
    I.e. allow games to load files *before* the PWAD, for load-order sensitivity. I am probably going to rename this from "auto" to "preload" because there should be a command-line parameter for this and `/AUTO` is not a good choice.
    
    Also added more error messages.

--- launcher/test_launcher.bat
+++ launcher/test_launcher.bat
@@ -8,7 +8,7 @@ REM # if that errored, pause to be able to show the error message
 IF ERRORLEVEL 1 POPD & PAUSE & GOTO:EOF
 
 REM # compiled successfully, now run it
-..\PortaDOOM\files\launcher.exe /DEBUG /PWAD test1 /PWAD test2
+..\PortaDOOM\files\launcher.exe /DEBUG blob
 
 IF %ERRORLEVEL% GT 0 (
 	ECHO: & ECHO launcher.exe terminated with status code %ERRORLEVEL%.
