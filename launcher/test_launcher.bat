5bbfa3ad646ce14d345528097b12048724205e4a
Kroc Camen <kroc@camendesign.com>
2018-10-14 22:59:08 +0100

    Enable `/REQ` for launcher
    This begins our work replacing "play.bat".

--- launcher/test_launcher.bat
+++ launcher/test_launcher.bat
@@ -8,7 +8,7 @@ REM # if that errored, pause to be able to show the error message
 IF ERRORLEVEL 1 POPD & PAUSE & GOTO:EOF
 
 REM # compiled successfully, now run it
-..\PortaDOOM\files\launcher.exe /DEBUG /DOOM2 /32
+..\PortaDOOM\files\launcher.exe /DEBUG /REQ x /DOOM2
 
 IF %ERRORLEVEL% GT 0 (
 	ECHO: & ECHO launcher.exe terminated with status code %ERRORLEVEL%.
