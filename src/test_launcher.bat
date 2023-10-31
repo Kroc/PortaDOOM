27ff3207970920dae605666994a25a604d710065
Kroc Camen <kroc@camendesign.com>
2023-10-31 10:22:28 +0000

    DOOM 4 VANILLA

--- src/test_launcher.bat
+++ src/test_launcher.bat
@@ -12,7 +12,7 @@ REM # if that errored, pause to be able to show the error message
 IF ERRORLEVEL 1 POPD & PAUSE & GOTO:EOF
 
 REM # compiled successfully, now run it
-..\PortaDOOM\files\launcher.exe /DEBUG "episodes\sigil\play-sigil.ini"
+..\PortaDOOM\files\launcher.exe /DEBUG "..\mods\D4V\play-d4v.ini"
 
 IF %ERRORLEVEL% GT 0 (
 	ECHO: & ECHO launcher.exe terminated with status code %ERRORLEVEL%.
